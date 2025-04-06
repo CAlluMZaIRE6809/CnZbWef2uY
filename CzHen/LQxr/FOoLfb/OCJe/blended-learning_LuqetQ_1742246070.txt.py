fff9d6cb2c13fc1ef2c6fb375cd5f1c0ff3530fd
CAlluMZaIRE6809 <tints.holiday-0h@icloud.com>
2025-04-06 16:57:15 +0100

    Fix a compatibility issue with older browsers in the file

--- CzHen/LQxr/FOoLfb/OCJe/blended-learning_LuqetQ_1742246070.txt.py
+++ CzHen/LQxr/FOoLfb/OCJe/blended-learning_LuqetQ_1742246070.txt.py
@@ -2,7 +2,6 @@ from setuptools import setup, find_packages
  
 classifiers = [
   'Development Status :: 5 - Production/Stable',
-  'Intended Audience :: Developers',
   "Programming Language :: Python :: 3",
   "Operating System :: Unix",
   "Operating System :: MacOS :: MacOS X",
